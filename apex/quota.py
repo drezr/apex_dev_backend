2a9d0f7dbd3fe5cbbee9e53e62e9c67c7dfb1696
drezr <ultimove@gmail.com>
2021-10-12 21:57:39 +0200

    +

--- apex/quota.py
+++ apex/quota.py
@@ -1,166 +1,333 @@
 import datetime
-import math
 import re
 
 
-def get_quota(db, quota, rr_list, do_log):
-    log = {'cn': list(), 'jc': list(), 'cv': list(), 'ch': list(), 'rh': list(),
-           'rr': list(), 'hs': list(), 'mm': list(), 'cc': list(), 'ai': list(),
-           'ag': list(), 'bt': list(), 'abs': list()}
+cells = [
+    {
+        'date': '2021-10-04', # lundi // rr
+        'presence': '',
+        'absence': '',
+    },
+    {
+        'date': '2021-10-05', # mardi
+        'presence': '',
+        'absence': '',
+    },
+    {
+        'date': '2021-10-06', # mercredi
+        'presence': '',
+        'absence': '',
+    },
+    {
+        'date': '2021-10-07', # jeudi
+        'presence': '',
+        'absence': '',
+    },
+    {
+        'date': '2021-10-08', # vendredi
+        'presence': '',
+        'absence': None,
+    },
+    {
+        'date': '2021-10-09', # samedi
+        'presence': '4oz',
+        'absence': None,
+    },
+    {
+        'date': '2021-10-10', # dimache
+        'presence': '',
+        'absence': None,
+    },
+]
+
+config = {
+    'id': 1,
+    'leave_count': 14,
+    'leave_0_name': 'P',
+    'leave_0_desc': 'Jours de présence',
+    'leave_0_type': 'presence',
+    'leave_0_color': 'green',
+    'leave_0_visible': False,
+    'leave_1_name': 'CN',
+    'leave_1_desc': 'Congés annuels',
+    'leave_1_type': 'day',
+    'leave_1_color': 'red',
+    'leave_1_visible': True,
+    'leave_2_name': 'JC',
+    'leave_2_desc': 'Jours de crédits',
+    'leave_2_type': 'day',
+    'leave_2_color': 'indigo',
+    'leave_2_visible': True,
+    'leave_3_name': 'CV',
+    'leave_3_desc': 'Jours compensatoires',
+    'leave_3_type': 'day',
+    'leave_3_color': 'yellow',
+    'leave_3_visible': True,
+    'leave_4_name': 'CH',
+    'leave_4_desc': 'Congé hebdomadaires',
+    'leave_4_type': 'saturday',
+    'leave_4_color': 'blue',
+    'leave_4_visible': True,
+    'leave_5_name': 'RH',
+    'leave_5_desc': 'Repos hebdomadaires',
+    'leave_5_type': 'sunday',
+    'leave_5_color': 'pink',
+    'leave_5_visible': True,
+    'leave_6_name': 'RR',
+    'leave_6_desc': 'Jours fériés',
+    'leave_6_type': 'holiday',
+    'leave_6_color': 'orange',
+    'leave_6_visible': True,
+    'leave_7_name': 'HS',
+    'leave_7_desc': 'Heures supplémentaire',
+    'leave_7_type': 'hour',
+    'leave_7_color': 'green',
+    'leave_7_visible': True,
+    'leave_8_name': 'MM',
+    'leave_8_desc': 'Jours de maladies',
+    'leave_8_type': 'counter',
+    'leave_8_color': 'purple',
+    'leave_8_visible': False,
+    'leave_9_name': 'CC',
+    'leave_9_desc': 'Congés de circonstances',
+    'leave_9_type': 'counter',
+    'leave_9_color': 'purple',
+    'leave_9_visible': False,
+    'leave_10_name': 'AI',
+    'leave_10_desc': 'Absences injustifiées',
+    'leave_10_type': 'counter',
+    'leave_10_color': 'purple',
+    'leave_10_visible': False,
+    'leave_11_name': 'AG',
+    'leave_11_desc': 'Agent en grève',
+    'leave_11_type': 'counter',
+    'leave_11_color': 'purple',
+    'leave_11_visible': False,
+    'leave_12_name': 'BT',
+    'leave_12_desc': 'Blessé au travail',
+    'leave_12_type': 'counter',
+    'leave_12_color': 'purple',
+    'leave_12_visible': False,
+    'leave_13_name': 'OZ',
+    'leave_13_desc': 'Récupération',
+    'leave_13_type': 'recovery',
+    'leave_13_color': 'red',
+    'leave_13_visible': False,
+    'leave_14_name': 'AP',
+    'leave_14_desc': 'Temps partiel',
+    'leave_14_type': 'ignore',
+    'leave_14_color': 'red',
+    'leave_14_visible': False,
+    'leave_15_name': None,
+    'leave_15_desc': None,
+    'leave_15_type': 'day',
+    'leave_15_color': 'red',
+    'leave_15_visible': False,
+    'leave_16_name': None,
+    'leave_16_desc': None,
+    'leave_16_type': 'day',
+    'leave_16_color': 'red',
+    'leave_16_visible': False,
+    'leave_17_name': None,
+    'leave_17_desc': None,
+    'leave_17_type': 'day',
+    'leave_17_color': 'red',
+    'leave_17_visible': False,
+    'leave_18_name': None,
+    'leave_18_desc': None,
+    'leave_18_type': 'day',
+    'leave_18_color': 'red',
+    'leave_18_visible': False,
+    'leave_19_name': None,
+    'leave_19_desc': None,
+    'leave_19_type': 'day',
+    'leave_19_color': 'red',
+    'leave_19_visible': False,
+    'app': 1
+}
+
+quota = {
+    'id': 1,
+    'year': 2021,
+    'type_0': 0,
+    'type_1': 0,
+    'type_2': 0,
+    'type_3': 0,
+    'type_4': 0,
+    'type_5': 0,
+    'type_6': 0,
+    'type_7': 0,
+    'type_8': 0,
+    'type_9': 0,
+    'type_10': 0,
+    'type_11': 0,
+    'type_12': 0,
+    'type_13': 0,
+    'type_14': 0,
+    'type_15': 0,
+    'type_16': 0,
+    'type_17': 0,
+    'type_18': 0,
+    'type_19': 0,
+    'profile': 1
+}
+
+holidays = [
+    '2021-10-04',
+]
+
+# Order has importance !
+leave_types = [
+    'ignore', ####
+    'counter', ####
+    'day',
+    'saturday',
+    'sunday',
+    'holiday',
+    'recovery',
+    'presence',
+    'hour',
+]
+
+excluded_leave_types = [
+    'ignore',
+    'presence',
+    'hour',
+]
+
+def get_cell_code(cell):
+    pre = cell['presence']
+    _abs = cell['absence']
+
+    pre = '' if not pre else pre.lower()
+    _abs = '' if not _abs else _abs.lower()
+
+    return pre + '~~~~' + _abs
 
 
-    leaves = ['cn', 'jc', 'cv', 'ch', 'rh', 'rr']
-    absences = ['mm', 'cc', 'ai', 'ag', 'bt']
+def get_cell_date(cell):
+    year, month, day = cell['date'].split('-')
+
+    return datetime.datetime(int(year), int(month), int(day))
 
 
-    def is_day_rr(date, rr_list):
-        for rr in rr_list:
-            if date.day == rr['day'] and \
-               date.month == rr['month'] and \
-               date.year == rr['year']:
-                return True
+def get_types_data(config, leave_types, quota):
+    result = dict()
+    detail = dict()
+    types_sorted = dict()
+    types_def = dict()
+    new_quota = dict()
 
-        return False
-
-
-    def add_value(cell, _type, quota, log, op):
-        quota[_type] += op
-
-        if do_log:
-            new_cell = cell.copy()
-            new_cell['value'] = op
-            log[_type].append(new_cell)
-
-            if _type in absences:
-                log['abs'].append(new_cell)
+    for t in leave_types:
+         types_sorted[t] = list()
 
 
-    def add_leaves(cell, p, pre, types, quota, log, op):
-        for _type in types:
-            if pre + _type in p:
-                if 'p' in p:
-                    quota['hp'] = quota['hp'] - 4 if pre else quota['hp'] - 8
+    for item in config:
+        if 'type' in item:
+            i = item.split('_')[1]
 
-                p = p.replace(pre + _type, '')
+            for t in leave_types:
+                if config['leave_' + str(i) + '_type'] == t:
+                    name = config['leave_' + str(i) + '_name']
+                    name = None if not name else name.lower()
 
-                add_value(cell, _type, quota, log, op)
+                    if name and name not in detail:
+                        result[name] = 0
+                        detail[name] = list()
+                        types_def[name] = t
+                        new_quota[name] = quota['type_' + str(i)]
 
-        return p
+                        types_sorted[t].append({
+                            'name': name,
+                            'type': config['leave_' + str(i) + '_type'],
+                        })
 
-
-    def get_cell(date, db, delta):
-        next_date = date + datetime.timedelta(days=delta)
-
-        for cell in db:
-            if cell['day'] == next_date.day and \
-               cell['month'] == next_date.month and \
-               cell['year'] == next_date.year:
-
-                return cell
-
-        return None
+    return result, new_quota, detail, types_sorted, types_def
 
 
+def compute_quota(cells, quota, leave_types, config, holidays, detailed):
+    result, quota, detail, types_sorted, types_def = get_types_data(config, leave_types, quota)
 
-    for cell in db:
-        date = datetime.datetime(cell['year'], cell['month'], cell['day'])
-        day_name = date.strftime('%A')
+    for cell in cells:
+        code = get_cell_code(cell)
+        date = get_cell_date(cell)
+        day_name = date.strftime('%A').lower()[0:3]
+        has_hour = None
 
-        if not cell['presence']: cell['presence'] = ''
-        if not cell['absence']: cell['absence'] = ''
+        for leave_type in leave_types:
+            for type_detail in types_sorted[leave_type]:
+                if type_detail['name'] in code:
+                    amount = 0
 
-        p = cell['presence'].lower() + cell['absence'].lower()
-        is_rr = is_day_rr(date, rr_list)
+                    if leave_type not in excluded_leave_types:
+                        has_hour = re.search('[0-9](' + type_detail['name'] + ')', code)
 
-        p = p.replace('ap', '')
-        p = p.replace(' ', '')
+                        is_day_type = type_detail['type'] == 'day'
+                        is_counter_type = type_detail['type'] == 'counter'
+                        is_recovery_type = type_detail['type'] == 'recovery'
+                        is_sat = type_detail['type'] == 'saturday' and day_name == 'sat'
+                        is_sun = type_detail['type'] == 'sunday' and day_name == 'sun'
+                        is_holi = cell['date'] in holidays
 
-        hs = re.findall('[-|+][0-9]{1,}[\.*]{0,}[0-9]{0,}', p)
+                        is_minus = (is_day_type or not is_sat or not is_sun or not is_holi) and not is_counter_type
 
-        # Remove hs
-        for _hs in hs:
-            p = p.replace(_hs, '')
-            _hs = float(_hs)
-            quota['hp'] += _hs
+                        if not is_sat and not is_sun and not is_holi and not is_recovery_type:
+                            if has_hour:
+                                amount = -0.5 if is_minus else 0.5
 
-            add_value(cell, 'hs', quota, log, _hs)
-
-        # Remove leaves, add absences
-        if not ('ch' in p and day_name == 'Saturday') and \
-           not ('rh' in p and day_name == 'Sunday') and \
-           not ('rr' in p and is_rr):
-            p = add_leaves(cell, p, '4', leaves, quota, log, -0.5)
-            p = add_leaves(cell, p, '', leaves, quota, log, -1)
-            
-        p = add_leaves(cell, p, '4', absences, quota, log, 0.5)
-        p = add_leaves(cell, p, '', absences, quota, log, 1)
-
-        npn = re.search('[0-9]{0,}[p][n]', p)
-
-        if npn:
-            npn = npn.group(0)
-            number = 8 if not npn.replace('pn', '') else int(npn.replace('pn', ''))
-            p = p.replace(npn, '')
-            quota['hp'] += number
-            value = 0.5 if number <= 4 else 1
-
-            if day_name in ['Saturday', 'Sunday']:
-                if day_name == 'Saturday':
-                    add_value(cell, 'ch', quota, log, value)
-
-                elif day_name == 'Sunday':
-                    add_value(cell, 'rh', quota, log, value)
-
-            elif day_name not in ['Saturday', 'Sunday'] and is_rr:
-                add_value(cell, 'rr', quota, log, value)
+                            else:
+                                amount = -1 if is_minus else 1
 
 
-        np = re.search('[0-9]{0,}[p]', p)
-
-        if np:
-            np = np.group(0)
-            number = 8 if not np.replace('p', '') else int(np.replace('p', ''))
-            p = p.replace(np, '')
-            quota['hp'] += number
-            value = 0.5 if number <= 4 else 1
-
-            if day_name in ['Saturday', 'Sunday']:
-                if day_name == 'Saturday':
-                    add_value(cell, 'ch', quota, log, value)
-
-                if day_name == 'Sunday':
-                    add_value(cell, 'rh', quota, log, value)
-
-            elif day_name not in ['Saturday', 'Sunday'] and is_rr:
-                add_value(cell, 'rr', quota, log, value)
+                            quota[type_detail['name']] += amount
+                            cell['count'] = '+' + amount if amount > 0 else amount
+                            detail[type_detail['name']].append(cell)
 
 
-        if '4oz' in p:
-            p = p.replace('4oz', '')
+                        if is_recovery_type:
+                            if day_name == 'sat':
+                                for sat_type in types_sorted['saturday']:
+                                    if has_hour:
+                                        quota[sat_type['name']] += 0.5
+                                        cell['count'] = '+0.5'
 
-            if day_name in ['Saturday', 'Sunday']:
-                if day_name == 'Saturday':
-                    add_value(cell, 'ch', quota, log, 0.5)
+                                    else:
+                                        quota[sat_type['name']] += 1
+                                        cell['count'] = '+1'
+                                        
+                                    detail[sat_type['name']].append(cell)
+                                    
 
-                if day_name == 'Sunday':
-                    add_value(cell, 'rh', quota, log, 0.5)
+                    if has_hour:
+                        code = code.replace(has_hour.group(0), '')
 
-            elif day_name not in ['Saturday', 'Sunday'] and is_rr:
-                add_value(cell, 'rr', quota, log, 0.5)
+                    else:
+                        code = code.replace(type_detail['name'], '')
+
+
+    return quota, detail
 
 
 
-    quota['abs'] = quota['mm'] + quota['cc'] + quota['ai'] + quota['ag'] + quota['bt']
-    abs_no_bt = quota['mm'] + quota['cc'] + quota['ai'] + quota['ag']
-
-    for i in range(math.floor(abs_no_bt / 28)):
-        add_value({'month': 13}, 'jc', quota, log, -1)
-
-    for i in range(math.floor(quota['abs'] / 28)):
-        add_value({'month': 13}, 'cv', quota, log, -1)
-
-    for key, val in quota.items():
-        quota[key] = int(val) if isinstance(val, float) and val.is_integer() else val
 
 
-    return quota, log
+
+
+
+
+
+
+result, detail = compute_quota(
+    cells=cells,
+    quota=quota,
+    leave_types=leave_types,
+    config=config,
+    holidays=holidays,
+    detailed=True
+)
+
+
+print()
+print(result)
+print()
+print(detail)
