0a2ec604d61ecc2cad3bc7660d154f236d472f58
drezr <ultimove@gmail.com>
2022-01-26 11:21:24 +0100

    +

--- apex_columns_merge.py
+++ apex_columns_merge.py
@@ -21,3 +21,23 @@ for work in works:
 
                 print('Deleting column #' + str(column.id))
                 column.delete()
+
+
+    columns = WorkColumn.objects.filter(
+        work=work,
+        name='s460s',
+    )
+
+    if columns.count() > 1:
+        kept_column = columns[0]
+
+        for i, column in enumerate(columns.all()):
+            if i > 0:
+                for row in column.rows.all():
+                    print('Merging row #' + str(row.id))
+                    row.work_column = kept_column
+                    row.position = i
+                    row.save()
+
+                print('Deleting column #' + str(column.id))
+                column.delete()
