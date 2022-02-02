74adb3f32c010ac1706b80d1ccc0144d63308aa9
drezr <ultimove@gmail.com>
2022-02-02 14:27:34 +0100

    +

--- apex/views/profile.py
+++ apex/views/profile.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -9,7 +10,10 @@ class ProfileView(APIView):
 
     def get(self, request):
         username = request.query_params['username']
-        user = User.objects.get(username=username)
+        user = User.objects.filter(username=username).first()
+
+        if not user or request.user != user:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
 
         result = {
             'profile': ProfileSerializer(user.profile, context={
