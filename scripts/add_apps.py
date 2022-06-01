401b4e256b8294f8383f8d74f206a8105b7a8b9a
drezr <ultimove@gmail.com>
2022-06-01 13:39:32 +0200

    +

--- scripts/add_apps.py
+++ scripts/add_apps.py
@@ -1,6 +1,18 @@
 from apex.models import *
 
-team_ids = [136, 137]
+team_ids = [
+	138,
+	139,
+	140,
+	141,
+	142,
+	143,
+	144,
+	145,
+	146,
+	147,
+	148,
+	149,
 
 for team_id in team_ids:
 	for app_name in ['draft', 'planner', 'radium', 'watcher']:
